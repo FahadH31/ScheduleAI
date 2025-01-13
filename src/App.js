b0dcac33f9b974bdb4921e2f2d9b51ef835e13ff
Kevaunjh <kevaunjh@gmail.com>
2025-01-13 13:26:38 -0500

    Creating Rough Template

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="w-screen h-screen bg-slate-700 flex">
+      <div className="w-1/2 bg-gray-500 h-full p-5 flex justify-center">
+        <div className="flex flex-col w-full">
+          <h1 className="text-center text-2xl font-bold text-white">
+            Create a new Schedule!
+          </h1>
+          <div className="bg-white flex-grow rounded-xl mt-2 flex justify-center items-center">
+            Schedule addition options
+          </div>
+          <div className="flex gap-4 ">
+            <button className="bg-white p-2 rounded-2xl w-full mt-3">
+              Submit
+            </button>
+          </div>
+        </div>
+      </div>
+      <div className="w-1/2 bg-slate-800 h-full p-5 flex justify-center ">
+        <div className="flex flex-col w-full">
+          <div className="text-white text-2xl text-center font-bold ">
+            Open AI Logo and Title
+          </div>
+          <div className="bg-gray-600 flex-grow rounded-xl mt-2 flex justify-center items-center">
+            Text for Chatbot
+          </div>
+          <div>
+            <form>
+              <textarea
+                className="bg-white p-2 rounded-2xl w-full mt-3 h-24 text-sm resize-none overflow-y-auto custom-scrollbar"
+                name="chati"
+                placeholder="Talk to your helper here!"
+                rows="3"
+              ></textarea>
+            </form>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
