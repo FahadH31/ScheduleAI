f29edb27f5d780c6ad6f28ec8a94afd11f88ee55
FahadH31 <129327022+FahadH31@users.noreply.github.com>
2026-01-25 19:24:07 -0500

    Iframe updates

--- frontend/src/components/GoogleCalendarIFrame.js
+++ frontend/src/components/GoogleCalendarIFrame.js
@@ -17,13 +17,23 @@ function GoogleCalendarIFrame() {
     )
 
     return (
-        <iframe
-            title="Calendar"
-            src={`https://calendar.google.com/calendar/embed?src=${email_address}&showPrint=0&showCalendars=0&ctz=${user_timezone}`}
-            className="sm:w-[90%] h-full"
-            id="calendarIFrame"
-        >
-        </iframe>
+        <div className="relative overflow-hidden w-full h-[full] border border-gray-200 rounded-lg">
+            <iframe
+                title="Calendar"
+                src={`https://calendar.google.com/calendar/embed?src=${email_address}&showPrint=0&showCalendars=0&showTabs=0&ctz=${user_timezone}`}
+                className="w-full h-full border-none"
+                id="calendarIFrame"
+            ></iframe>
+            <div 
+                className="absolute bottom-0 left-0 w-[200px] h-[26px] bg-[#F0F4F9] z-10"
+                aria-hidden="true"
+            ></div>
+
+            <div
+                className="absolute top-0 right-0 w-[40px] h-[40px] bg-[#F0F4F9] z-10"
+                aria-hidden="true"
+            ></div>
+        </div>
     );
 }
 
